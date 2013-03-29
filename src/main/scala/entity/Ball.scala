144cba2425c1bad77dffaa252ce9b0e50f873d16
ramn <github@rymdimperiet.org>
2013-03-29 22:42:56 +0100

    Add moving ball

--- src/main/scala/entity/Ball.scala
+++ src/main/scala/entity/Ball.scala
@@ -6,19 +6,33 @@ import org.newdawn.slick.state.StateBasedGame
 import org.newdawn.slick.Color
 import org.newdawn.slick.Graphics
 import org.newdawn.slick.Input
-import org.newdawn.slick.Music
-import org.newdawn.slick.Image
-import org.newdawn.slick.SpriteSheet
-import org.newdawn.slick.geom.{Shape, Rectangle}
+import org.newdawn.slick.geom.{Shape, Circle}
+import org.newdawn.slick.geom.Vector2f
+
+import se.ramn.krossaklossar.KrossaKlossar.{Width, Height}
 
 
 class Ball extends Collidable {
+  private val radius = 4
+  private val initX = Width/2-radius/2
+  private val initY = Height/2
+  private val initialSpeed = 0.1f
+  private val speed = initialSpeed
+  private val position = new Vector2f(initX, initY)
+  private val shape: Shape = new Circle(initX, initY, radius)
+  private val direction = new Vector2f(1.0f, 1.0f)
 
   def update(gc: GameContainer, game: StateBasedGame, delta: Int) {
+    //position.x += direction.x * delta * speed
+    //position.y -= direction.y * delta * speed
+    position.add(direction.copy.scale(speed * delta))
+    shape.setLocation(position)
   }
 
   def render(g: Graphics) {
+    g.setColor(Color.red)
+    g.fill(shape)
   }
 
-  override def collisionShape = ???
+  override def collisionShape = shape
 }
